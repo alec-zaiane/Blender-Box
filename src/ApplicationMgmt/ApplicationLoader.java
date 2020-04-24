2ee33ff44d35e56a456c2f36a9c2a0c06a14d4f1
Alec Zaiane <50636059+alec-zaiane@users.noreply.github.com>
2020-04-23 20:57:39 -0600

    Optimized imports

--- src/ApplicationMgmt/ApplicationLoader.java
+++ src/ApplicationMgmt/ApplicationLoader.java
@@ -1,7 +1,6 @@
 package ApplicationMgmt;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.io.File;
 
@@ -17,7 +16,7 @@ public class ApplicationLoader {
      * @return list of usable blender builds in directory
      * @throws FileNotFoundException when dir is not found, not readable, or not a directory
      */
-    public static ArrayList<Build> loadBuildsFromDir(File dir) throws FileNotFoundException,IOException {
+    public static ArrayList<Build> loadBuildsFromDir(File dir) throws FileNotFoundException {
         //confirm the input is a directory, readable, and exists
         if (!dir.exists()) throw new FileNotFoundException("File \"" + dir.getAbsolutePath() + "\" does not exist");
         if (!dir.canRead()) throw new FileNotFoundException("File \"" + dir.getAbsolutePath() + "\" is not readable");
@@ -26,7 +25,7 @@ public class ApplicationLoader {
         //test for "blender.exe" in the directory, and for other subdirectories
         //ignore directories called "datafiles","python","scripts"
         File[] fileList = dir.listFiles();
-        if(fileList == null) throw new IOException("FileList was Null");
+        assert fileList != null;
         for (File file : fileList) {
             if (file.isDirectory()) {
                 String name = file.getName();
--- src/Blender_Box_Main.java
+++ src/Blender_Box_Main.java
@@ -3,7 +3,6 @@ import ApplicationMgmt.Build;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,16 +11,13 @@ public class Blender_Box_Main {
     public static void main(String[] args) {
         Scanner wordScan = new Scanner(System.in);
         Scanner numScan = new Scanner(System.in);
-        ArrayList<Build> builds = new ArrayList<Build>();
+        ArrayList<Build> builds = new ArrayList<>();
         try {
             builds = ApplicationLoader.loadBuildsFromDir(new File("F:\\Blender versions"));
             System.out.println(builds.size());
         }
         catch (FileNotFoundException e){
             System.out.println("FileNotFoundException");
-            e.printStackTrace();
-        }catch (IOException e){
-            System.out.println("IOException");
             e.printStackTrace();
         }
         System.out.println("what index would you like to run?");
