bfc5730d80887b4b53cba26df8f291522a6898a6
Alec Zaiane <50636059+alec-zaiane@users.noreply.github.com>
2020-04-25 11:58:44 -0600

    Catch Security exception when blender build is not properly readable

--- src/ApplicationMgmt/Build.java
+++ src/ApplicationMgmt/Build.java
@@ -14,14 +14,19 @@ public class Build {
 
     /**
      * Creates a new build object from the executable's location
-     *
      * @param location location of blender.exe for this build
      */
     public Build(File location) {
         this.location = location;
         if (this.location.getName().equals("blender.exe") && this.location.isFile())
             usable = true; //confirm the location is a blender file
-        this.Time = location.lastModified();
+        try {
+            this.Time = location.lastModified();
+        } catch (SecurityException e){
+            //TODO implement logging
+            System.out.println("Security Exception, time not readable");
+            e.printStackTrace();
+        }
     }
 
     public void run() {
