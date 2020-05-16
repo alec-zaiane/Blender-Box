c826d8cb589150bb014a2235b99b53d376011471
Alec Zaiane <50636059+alec-zaiane@users.noreply.github.com>
2020-05-15 21:29:21 -0600

    initial commit

---
+++ src/ProjectMgmt/BlendManager.java
@@ -0,0 +1,14 @@
+package ProjectMgmt;
+
+import java.util.ArrayList;
+
+public class BlendManager {
+    public static ArrayList<Blend> allLoadedBlends = new ArrayList<>();
+
+    public static void loadBlends(){
+
+    }
+    public static void saveBlends(){
+
+    }
+}
